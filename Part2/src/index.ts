2bc02df35327b3981236a950ce904e18285b610c
kimura-eagency <yuki.kimura.77777@gmail.com>
2022-05-21 15:38:32 +0900

    Update index.ts

--- Part2/src/index.ts
+++ Part2/src/index.ts
@@ -239,6 +239,21 @@ const encrypt = async (
 ): Promise<Ciphertext> => {
   const mimc7 = await buildMimc7();
   // [assignment] generate the IV, use Mimc7 to hash the shared key with the IV, then encrypt the plain text
+  const iv = buf2Bigint(mimc7.multiHash(plaintext, BigInt(0)))
+  const ciphertext: Ciphertext = {
+        iv,
+        data: plaintext.map((e: BigInt, i: number): bigint => {
+          let a = buf2Bigint(bigInt2Buffer(e))
+          let b = buf2Bigint(mimc7.hash(
+            sharedKey,
+            iv + BigInt(i),
+        ))
+            return a+b
+        }),
+    }
+
+    // TODO: add asserts here
+    return ciphertext
 };
 
 /*
@@ -250,6 +265,14 @@ const decrypt = async (
   sharedKey: EcdhSharedKey,
 ): Promise<Plaintext> => {
   // [assignment] use Mimc7 to hash the shared key with the IV, then descrypt the ciphertext
+  const mimc7 = await buildMimc7();
+  const plaintext: Plaintext = ciphertext.data.map(
+    (e: BigInt, i: number): bigint => {
+        return buf2Bigint(bigInt2Buffer(e)) - buf2Bigint(mimc7.hash(sharedKey, BigInt(buf2Bigint(bigInt2Buffer(ciphertext.iv))) + BigInt(i)))
+    }
+  )
+
+  return plaintext
 };
 
 export {
